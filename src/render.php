28780f4a1bab28774295a9a38950a7cfa916b92b
KJ Roelke <kj.roelke@gmail.com>
2025-11-11 12:25:38 -0600

    fix breadcrumbs rendering

--- src/render.php
+++ src/render.php
@@ -10,11 +10,11 @@
 use MacrosBySara\Blocks\Breadcrumbs;
 
 
-$breadcrumbs = new Breadcrumbs( $attributes,get_the_category( get_the_ID() ) );
+$breadcrumbs = new Breadcrumbs( $attributes, get_the_category( get_the_ID() ) );
 ?>
 
 <nav <?php echo get_block_wrapper_attributes(); ?> aria-label="Breadcrumb">
-    <ol class="breadcrumb-list">
-        <?php $breadcrumbs->the_breadcrumbs(); ?>
-    </ol>
+	<ol class="breadcrumb-list">
+		<?php $breadcrumbs->the_breadcrumbs(); ?>
+	</ol>
 </nav>
